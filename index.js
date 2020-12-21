4984cb93400077c6a87b4bf6dd1ae321e1996922
irving quevedo <irvingquevedo@irvings-MacBook-Pro.local>
2020-12-21 12:26:49 -0500

    final product

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
 
-axios.get('https://pokeapi.co/api/v2/type/13 ')
-.then(res => {
-   let pokeDataJSON = JSON.stringify(res.data);
-    fs.writeFile('electric-pokemon.json', pokeDataJSON,  e => {
-        console.log(e)
-    })
-}).catch (error => {
-    console.log(error)
+axios.get("https://pokeapi.co/api/v2/type").then((res) => {
+  let pokemonTypes = res.data.results.map((pokemon) => pokemon.name);
+  console.log(pokemonTypes);
 });
 
-
-axios.get('https://pokeapi.co/api/v2/type')
-.then(res => {
-    let pokemonTypes = res.data.results.map(pokemon => pokemon.name);
-    console.log(pokemonTypes)
-});
-
-console.log(process.argv);
-let type = process.argv[2];
-
-
-
+if (process.argv[2]) {
+  axios.get("https://pokeapi.co/api/v2/type").then((res) => {
+    let pokemonTypes = res.data.results.map((pokemon) => pokemon.name);
+    let pokeIndex = pokemonTypes.indexOf(process.argv[2]);
+    let id = pokeIndex + 1;
+    axios.get("https://pokeapi.co/api/v2/type/" + id).then((res) => {
+      res.data.pokemon.forEach((item) => {
+        console.log(`Pokemon name: ${item.pokemon.name}`);
+        console.log(`Pokemon url: ${item.pokemon.url}`);
+      });
+    });
+  });
+} else {
+  console.log(
+    "input a type to see a list of pokemons names and URL's example node index.js 'type' "
+  );
+}
